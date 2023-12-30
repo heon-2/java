4f999514a957e0966948e4d0f6257e9a11e6f4dd
이지헌 <jiheon_114@naver.com>
2023-12-30 22:02:00 +0900

    add java practice code

---
+++ src/operator/Operator1.java
@@ -0,0 +1,26 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+        // 변수 초기화
+        int a = 5 ;
+        int b = 2;
+
+        // 덧셈
+        int sum = a + b;
+        System.out.println("a + b = " + sum); // 출력 a + b = 7
+
+        // 뺄셈
+        int diff = a-b;
+        System.out.println("a - b = " + diff);
+
+        // 곱셈
+        int mulit = a * b ;
+        //나눗셈
+        int div = a / b; // 2
+        // 나머지
+        int mod = a % b; // 5/2 2,1
+
+
+    }
+}
